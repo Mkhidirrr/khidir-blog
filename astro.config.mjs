57c549587ee9338ac42d59a099255c2323cc06e7
Mkhidirrr <mochamadkhidir10@gmail.com>
2025-06-03 06:01:28 +0700

    fix: errorer

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,11 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import vercel from '@astrojs/vercel/static';
 import react from '@astrojs/react';
-import prefetch from '@astrojs/prefetch';
 import robotsTxt from 'astro-robots-txt';
 import compress from 'astro-compress';
+import rehypeSlug from 'rehype-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeExternalLinks from 'rehype-external-links';
 
 export default defineConfig({
   site: 'https://khidir.dev',
@@ -15,15 +17,14 @@ export default defineConfig({
   integrations: [
     mdx({
       rehypePlugins: [
-        'rehype-slug',
-        ['rehype-autolink-headings', { behavior: 'append' }],
-        ['rehype-external-links', { target: '_blank', rel: ['noopener', 'noreferrer'] }]
+        rehypeSlug,
+        [rehypeAutolinkHeadings, { behavior: 'append' }],
+        [rehypeExternalLinks, { target: '_blank', rel: ['noopener', 'noreferrer'] }]
       ]
     }),
     tailwind(),
     sitemap(),
     react(),
-    prefetch(),
     robotsTxt(),
     compress()
   ],
