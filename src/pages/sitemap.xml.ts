eac462469fe8fae293b8bc3e5079a4bf00dda7a4
khidir <khidir@khidir.lan>
2025-12-07 03:32:51 -0500

    update: isi isi bagian penting

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -16,6 +16,19 @@ export async function GET({ site }: APIContext) {
   });
 }
 
-function generateSitemap(site, posts) {
-  // ...existing code...
+function generateSitemap(site: string, posts: any[]) {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${posts
+        .map(
+          (post) => `
+        <url>
+          <loc>${new URL(`/${post.collection}/${post.slug}/`, site)}</loc>
+          <lastmod>${post.data.date}</lastmod>
+          <priority>0.8</priority>
+          <changefreq>weekly</changefreq>
+        </url>`
+        )
+        .join('')}
+    </urlset>`;
 }
