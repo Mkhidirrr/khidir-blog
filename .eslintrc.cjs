389b83a7dba3d5f51318f5225b5c55ea0adffb6f
khidir <khidir@khidir.lan>
2025-12-06 23:40:51 -0500

    fix: perbaikan error

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,5 @@
+// ANALYSIS: File ini berisi konfigurasi ESLint. Pastikan rules sudah sesuai dengan kebutuhan project Astro/TypeScript. Periksa apakah ada plugin yang belum diinstall atau aturan yang bertentangan.
+
 module.exports = {
   extends: [
     'plugin:@typescript-eslint/recommended',
